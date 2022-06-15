27236e00c289a45d6882e6aaf096b8d60c50dcb0
Ben Holgate <holgateb@hotmail.com>
2022-06-14 18:39:59 -0700

    added for loop to generate random password.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var generateBtn = document.querySelector("#generate");
+
 var numbers = "1234567890".split("");
 var lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
@@ -12,20 +13,38 @@ function writePassword() {
 
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword(){
+function generatePassword() {
+var characters = []
+var length = prompt("How many characters would you like your password to be?");
 
-}
+if(length < 8 || length > 128)
+  alert("Password must be between 8 and 128 characters.");
 
-password = []
-
-var length = prompt("How many characters would you like your password to be?", "");
-
-if(length >= 8 && length <=128){
+if(length >= 8 && length <= 128) {
   var wantNumbers = window.confirm("Do you want to include numbers?");
   var wantUpper = window.confirm("Do you want to include upper case letters?");
   var wantLower = window.confirm("Do you want to include lower case letters?");
-  var wantSpecial = window.confirm("Do you want to include special characters?") ;
+  var wantSpecial = window.confirm("Do you want to include special characters?");
 }
 
+if(wantNumbers || wantUpper || wantLower || wantSpecial) {
+  if(wantNumbers)
+  characters = characters.concat(numbers);
+  if(wantUpper)
+  character = characters.concat(uppercase);
+  if(wantLower)
+  characters = characters.concat(lowercase);
+  if(wantSpecial)
+  characters = characters.concat(special);
+}
+
+for ( var i = 0, i < length, i++ ) {
+  var randomCharacter = math.floor(math.random() * characters.length);
+  characters.push(i)
+  // console.log(randomCharacter)
+}
+
+return(randomCharacter)
+}
 
 
